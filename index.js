5bdee8a1310291dd7bc07d9132e3dfbc987f5651
Ashley JP <ashleypereira70@gmail.com>
2018-11-07 21:40:52 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var name ="Joe"
+var height = "74"
+module.export = { name, height}
+
+if (name === "Susan") {
+  console.log("The name is correct")
+}
+
+console.log("Expected the name to be Susan")
+// Don't worry about this 
+module.exports = {name, height}
+
+var index = require("./index.js")
+window.word
+function sayHello() {
+  console.log('Hello!')
+}
+function sayHelloToIsabel() {
+  console.log("Hello, Isabel!")
+}
---
+++ other_file.js
@@ -0,0 +1,7 @@
+console.log(name)
+
+var index = require("./index.js")
+console.log(index.name)
+function sayHelloToSofia() {
+  console.log("Hello, Sofia!")
+}
